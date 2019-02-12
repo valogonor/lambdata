a87533b2b596ab14dd9d7abe7205daf078b87d11
Valerie Ogonor <vogonor1@students.kennesaw.edu>
2019-02-12 16:57:44 -0500

    "Add setup.py"

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/user/bin/env python
+''' Package setup/installation and metadata for lambdata
+'''
+
+import setuptools
+
+REQUIRED = [
+    "numpy"
+    "pandas"
+]
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setuptools.setup(
+    name="lambdata-valogonor",
+    version="0.0.1",
+    author="valogonor",
+    description="A collection of Data Science helper functions",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/valogonor/lambdata",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.7",
+    install_requires=REQUIRED,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+)

fd0637353c7a2f77bcc017c817c2271dbbe26430
Valerie Ogonor <vogonor1@students.kennesaw.edu>
2019-02-13 16:55:48 -0500

    Edited init to conform to pep8

--- lambdata_valogonor/__init__.py
+++ lambdata_valogonor/__init__.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
+
 def split_date(df, date_column):
     df.date_column = pd.to_datetime(df.date_column)
     df['year'] = pd.DatetimeIndex(df.date_column).year
@@ -10,16 +11,16 @@ def split_date(df, date_column):
 
 
 def train_validation_test_split(
-    X, y, train_size=0.8, val_size=0.1, test_size=0.1, 
-    random_state=None, shuffle=True):
-        
+        X, y, train_size=0.8, val_size=0.1, test_size=0.1,
+        random_state=None, shuffle=True):
+
     assert train_size + val_size + test_size == 1
-    
+
     X_train_val, X_test, y_train_val, y_test = train_test_split(
         X, y, test_size=test_size, random_state=random_state, shuffle=shuffle)
-    
+
     X_train, X_val, y_train, y_val = train_test_split(
-        X_train_val, y_train_val, test_size=val_size/(train_size+val_size), 
+        X_train_val, y_train_val, test_size=val_size/(train_size+val_size),
         random_state=random_state, shuffle=shuffle)
-    
+
     return X_train, X_val, X_test, y_train, y_val, y_test

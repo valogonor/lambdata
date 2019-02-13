cb72828015d025cbf2f3472b6c87132a7ba23c14
Valerie Ogonor <vogonor1@students.kennesaw.edu>
2019-02-13 17:22:08 -0500

    Changed version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lambdata-valogonor",
-    version="0.0.1",
+    version="0.0.2",
     author="valogonor",
     description="A collection of Data Science helper functions",
     long_description=LONG_DESCRIPTION,

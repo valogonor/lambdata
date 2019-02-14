4376419d0db8008194fb9eee2b8df0558bfd4f1a
Valerie Ogonor <vogonor1@students.kennesaw.edu>
2019-02-14 17:07:26 -0500

    Attempting to fix inability to install from pip.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 import setuptools
 
 REQUIRED = [
-    "numpy"
+    "sklearn",
     "pandas"
 ]
 
@@ -14,7 +14,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lambdata-valogonor",
-    version="0.0.2",
+    version="0.0.3",
     author="valogonor",
     description="A collection of Data Science helper functions",
     long_description=LONG_DESCRIPTION,
